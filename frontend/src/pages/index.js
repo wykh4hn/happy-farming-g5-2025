1423bbbd3457c855624772960a9a7ea5325b1148
Hoàng Minh Đức <hoangminhduc0308@gmail.com>
2025-05-30 11:33:39 +0700

    added basic pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,18 +3,13 @@
 import React from "react";
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
+import MainNav from "./nav";
 import "./styles.css";
 
 const Intro = () => {
   return (
-    <div className="App">
-      <nav>
-        <a href="">Home</a>
-        <a href="">other</a>
-        <button id="create-wallet">
-          <a href="">Create New Wallet</a>
-        </button>
-      </nav>
+    <div>
+      <MainNav />
       <h1>Welcome to our website!</h1>
       <p>Please login</p>
       <button id="login">Log in</button>
---
+++ frontend/src/pages/main.js
@@ -0,0 +1,40 @@
+import { MainNav } from "./nav";
+import React from "react";
+
+class Product extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "as",
+      price: 0,
+      description: "the",
+      detail: (
+        <button>
+          <a href="duckduckgo.com">detail</a>
+        </button>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>{this.state.name}</h4>
+        <p>{"$" + toString(this.state.price)}</p>
+        <p>{this.state.description}</p>
+        {this.state.detail}
+      </div>
+    );
+  }
+}
+
+const Main = () => {
+  return (
+    <div>
+      <MainNav />
+      <Product name="box" price={5} description="a box" />
+    </div>
+  );
+};
+
+export { Main };
