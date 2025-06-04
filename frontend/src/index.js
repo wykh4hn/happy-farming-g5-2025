33fe6154b3afa17df91cfce958db9168b6a1f5df
Hoàng Minh Đức <hoangminhduc0308@gmail.com>
2025-06-04 15:16:17 +0700

    added example chart to transaction_history

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./styles/styles.css";
 import "./index.css";
--- frontend/src/pages/trans_history.js
+++ frontend/src/pages/trans_history.js
@@ -6,13 +6,83 @@ import { MainNav } from "./nav";
 import { Footer } from "./footer";
 import { Sidebar } from "./sidebar";
 
+import * as d3 from "d3";
+import { useState, useEffect } from "react";
+import { useRef } from "react";
+// import
+
+const testX = [1, 2, 3, 4, 5, 6, 7];
+const testY = [];
+for (let i = 0; i < testX.length; i++) {
+  testY.push(Math.random() * 100);
+}
+
+const Chart = () => {};
+
 const TransactionHistory = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    // Clear previous chart
+    d3.select(chartRef.current).selectAll("*").remove();
+
+    // Set up dimensions
+    const width = 400;
+    const height = 200;
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+    // Create SVG
+    const svg = d3
+      .select(chartRef.current)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // Scales
+    const x = d3
+      .scaleLinear()
+      .domain([d3.min(testX), d3.max(testX)])
+      .range([margin.left, width - margin.right]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(testY)])
+      .range([height - margin.bottom, margin.top]);
+
+    // Line generator
+    const line = d3
+      .line()
+      .x((d, i) => x(testX[i]))
+      .y((d) => y(d));
+
+    // Draw line
+    svg
+      .append("path")
+      .datum(testY)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line);
+
+    // X Axis
+    svg
+      .append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(testX.length));
+
+    // Y Axis
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+  }, []);
   return (
     <div>
       <MainNav />
       <Sidebar />
       <div id="transaction-history" className="main-content">
         <p>Page in progress</p>
+        <div ref={chartRef}></div>
+        {/* <Chart /> */}
       </div>
       <Footer />
     </div>
