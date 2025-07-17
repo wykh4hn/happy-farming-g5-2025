81b5db76724f6bf40ddfa86696ac68f73eb24ad8
Hoàng Minh Đức <hoangminhduc0308@gmail.com>
2025-07-17 18:14:28 +0700

    (unfinished) attempt to fix home package + installing hardhat for no reason

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { MainNav } from "../components/nav"; 
-import "../styles/home.css"
+import { MainNav } from "../components/nav";
+import "../styles/home.css";
 
-{/*I add some parts of the css into this bc idk why all in css so lag and even effect on other pages */}
+{
+  /*I add some parts of the css into this bc idk why all in css so lag and even effect on other pages */
+}
 const Home = () => {
-
   const bgImage = `${process.env.PUBLIC_URL}/background.png`;
 
   const homeStyle = {
@@ -14,7 +15,6 @@ const Home = () => {
     backgroundPosition: "center",
     backgroundAttachment: "fixed",
     minHeight: "100vh",
-    
   };
 
   const introImageStyle = {
@@ -25,29 +25,37 @@ const Home = () => {
     borderRadius: "100%",
     border: "10px solid #095a5d",
     boxShadow: "30px 8px 20px rgba(109, 98, 49, 0.4)",
-    clipPath: "ellipse(100% 75% at center)"
+    clipPath: "ellipse(100% 75% at center)",
   };
 
-
-  {/*the main part */}
+  {
+    /*the main part */
+  }
 
   return (
     <>
       <MainNav />
       <div id="home-container" style={homeStyle}>
-        
         <div id="header">
-          <h1 style={{ fontFamily: '"Abril Fatface", sans-serif' }}>HAPPY FARMING</h1>
+          <h1 style={{ fontFamily: '"Abril Fatface", sans-serif' }}>
+            HAPPY FARMING
+          </h1>
         </div>
 
         <div id="description-intro">
-          <p style={{ margin: 0 }}>Fresh, new, clean products always here for you</p>
+          <p style={{ margin: 0 }}>
+            Fresh, new, clean products always here for you
+          </p>
         </div>
 
         <div className="image-wrapper">
-          <img id="intro-image" style={introImageStyle} src={`${process.env.PUBLIC_URL}/intro.png`} alt="Intro tho" />
+          <img
+            id="intro-image"
+            style={introImageStyle}
+            src={`${process.env.PUBLIC_URL}/intro.png`}
+            alt="Intro tho"
+          />
         </div>
-
       </div>
     </>
   );
